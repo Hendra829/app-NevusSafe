cdf38ab7a843bdfb3aca16cf1aa9fe96d6487435
Ar'Bon <kohcimmay@gmail.com>
2026-09-14 03:13:15 +0700

    test: import vault key store contract

--- mobile/test/encryption_service_test.dart
+++ mobile/test/encryption_service_test.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nevus_safe/services/encryption_service.dart';
+import 'package:nevus_safe/services/key_store.dart';
 
 class _MemoryKeyStore implements VaultKeyStore {
   List<int>? _key;
