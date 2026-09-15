6de7c5e4729f5c8aa892f3ca3285532b4341cf03
Ar'Bon <kohcimmay@gmail.com>
2026-09-15 17:21:10 +0700

    fix(policy): align API with safe memory limit

--- server/src/server.js
+++ server/src/server.js
@@ -19,7 +19,7 @@ const server = createServer((req, res) => {
   }
   if (req.method === 'GET' && req.url === '/v1/policy') {
     return json(res, 200, {
-      maxFileBytes: Number(process.env.MAX_FILE_BYTES || 67108864),
+      maxFileBytes: Number(process.env.MAX_FILE_BYTES || 33554432),
       uploadChunkBytes: Number(process.env.UPLOAD_CHUNK_BYTES || 8388608),
       driveScope: 'https://www.googleapis.com/auth/drive.file',
     });
